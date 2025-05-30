8fc9ac21fc8002ca43a8ce9e72d1d7a03b3e3389
Hugo Coto Florez <hugocoto100305@gmail.com>
2025-05-30 10:33:06 +0200

    adding src

---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TP_IMPLEMENTATION
+#include "tp.h"
+
+#define width 80
+#define height 24
+/* microseconds */
+#define sleep_time 100000
+
+#define A '#' // alive cell
+#define D ' ' // dead cell
+
+// clang-format off
+static char screen_buffer[width * height] = {
+      "                                                                                "
+      "                                                                                "
+      "                               #                                                "
+      "                             # #                                                "
+      "                   ##      ##            ##                                     "
+      "                  #   #    ##            ##                                     "
+      "       ##        #     #   ##                                                   "
+      "       ##        #   # ##    # #                                                "
+      "                 #     #       #                                                "
+      "                  #   #                                                         "
+      "                   ##                                                           "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+      "                                                                                "
+
+};
+// clang-format on
+
+static char next_buffer[width * height];
+
+void
+calc_cell(char *cell)
+{
+        ptrdiff_t pos = cell - next_buffer;
+        int h = pos / width;
+        int w = pos % width;
+        int lived_neightbours = 0;
+
+        int i, j, position;
+        for (i = -1; i <= 1; i++)
+                for (j = -1; j <= 1; j++) {
+                        if (i == 0 && j == 0) continue;
+                        if (h + i >= height || w + j >= width) continue;
+                        if (h + i < 0 || w + j < 0) continue;
+                        if (screen_buffer[(h + i) * width + w + j] != D) ++lived_neightbours;
+                }
+
+        switch (lived_neightbours) {
+        case 2:
+                *cell = screen_buffer[pos];
+                break;
+        case 3:
+                *cell = A;
+                break;
+        default:
+                *cell = D;
+                break;
+        }
+}
+
+void
+print_state()
+{
+        printf("\e[H");
+        for (int i = 0; i < height; i++) {
+                write(STDOUT_FILENO, screen_buffer + i * width, width);
+                write(STDOUT_FILENO, &"\n", 1);
+        }
+        fflush(stdout);
+}
+
+
+int
+main()
+{
+        printf("\e[H\e[2J");
+        fflush(stdout);
+        while (1) {
+                print_state();
+                usleep(sleep_time);
+
+                thread_pool(next_buffer, width * height, sizeof(char), calc_cell, 16);
+                memcpy(screen_buffer, next_buffer, sizeof screen_buffer / sizeof *screen_buffer);
+        }
+        return 0;
+}
---
+++ tp.h
@@ -0,0 +1,130 @@
+#ifndef TP_H_
+#define TP_H_
+
+#define TP_DEBUG 0
+#define NUM_THREADS 1
+#define CHUNK_SIZE 1024 * 1024
+
+/* Looks like the chunk size do not modify eficiency. */
+
+#include <assert.h>
+#include <pthread.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <threads.h>
+#include <time.h>
+#include <unistd.h>
+
+
+#ifdef TP_IMPLEMENTATION
+
+
+#if defined(TP_DEBUG) && TP_DEBUG > 0
+#define LOG(format, ...) printf(format, ##__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+typedef struct {
+        char *array;
+        void (*func)(void *);
+        ssize_t size;
+        ssize_t elemsize;
+        ssize_t *next_index;
+        pthread_mutex_t *mutex;
+} TPData;
+
+static inline ssize_t
+ask_for_offset(ssize_t *next_index, pthread_mutex_t *mutex, ssize_t arr_size)
+{
+        ssize_t ret;
+        LOG("Before ask_for_offset\n");
+        pthread_mutex_lock(mutex);
+        ret = *next_index;
+#if defined(CHUNK_SIZE) && CHUNK_SIZE > 1
+        *next_index += (CHUNK_SIZE);
+#else
+        ++*next_index;
+#endif
+        pthread_mutex_unlock(mutex);
+        LOG("After ask_for_offset\n");
+
+        if (ret < arr_size) {
+                LOG("ret: %zd\n", ret);
+                return ret;
+        } else {
+                LOG("ret: %zd < %zd\n", ret, arr_size);
+                return -1;
+        }
+}
+
+
+static void *
+tp_thread_routine(void *args)
+{
+        TPData data = *(TPData *) args;
+        ssize_t arr_ptr;
+#if defined(CHUNK_SIZE) && CHUNK_SIZE > 1
+        ssize_t end = (CHUNK_SIZE);
+#endif
+
+        while ((arr_ptr = ask_for_offset(data.next_index, data.mutex, data.size)) >= 0) {
+#if defined(CHUNK_SIZE) && CHUNK_SIZE > 1
+                LOG("iter++\n");
+
+                if (data.size < arr_ptr + (CHUNK_SIZE))
+                        end = data.size - arr_ptr;
+                LOG("Before for\n");
+                for (ssize_t offset = 0; offset < end; offset++)
+                        (data.func)((void *) ((char *) data.array + (arr_ptr + offset) * data.elemsize));
+#else
+                (data.func)((void *) (data.array + arr_ptr * data.elemsize));
+#endif
+                LOG("After for\n");
+        }
+
+        LOG("returning from tp_thread_routine\n");
+        return NULL;
+}
+
+#define thread_pool(arr, n, esize, f, ...)                                                        \
+        do {                                                                                      \
+                __auto_type __array__ = (arr);                                                    \
+                __auto_type __function__ = (f);                                                   \
+                ssize_t next_index = 0;                                                           \
+                ssize_t num_threads = NUM_THREADS;                                                \
+                __VA_OPT__(num_threads = __VA_ARGS__);                                            \
+                pthread_t threads[num_threads];                                                   \
+                pthread_mutex_t next_ptr_mutex;                                                   \
+                TPData data;                                                                      \
+                ssize_t i;                                                                        \
+                pthread_mutex_init(&next_ptr_mutex, NULL);                                        \
+                data = (TPData) {                                                                 \
+                        .array = (void *) (__array__),                                            \
+                        .func = (void (*)(void *))(__function__),                                 \
+                        .size = (n),                                                              \
+                        .elemsize = (esize),                                                      \
+                        .next_index = &next_index,                                                \
+                        .mutex = &next_ptr_mutex,                                                 \
+                };                                                                                \
+                for (i = 0; i < num_threads - 1; i++) {                                           \
+                        LOG("[CREATE] create new thread %zu\n", i);                               \
+                        assert(pthread_create(threads + i, NULL, tp_thread_routine, &data) == 0); \
+                }                                                                                 \
+                tp_thread_routine(&data);                                                         \
+                for (i = 0; i < num_threads - 1; i++) {                                           \
+                        assert(pthread_join(threads[i], NULL) == 0);                              \
+                        LOG("[JOINED] thread %zu\n", i);                                          \
+                }                                                                                 \
+                LOG("[JOINED] ALL\n");                                                            \
+                pthread_mutex_destroy(&next_ptr_mutex);                                           \
+                LOG("pthread_mutex_destroy \n");                                                  \
+        } while (0);
+
+#endif // TP_IMPLEMENTATION
+#endif // TP_H_
